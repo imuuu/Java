43dc184ccf62610ae06b9d22ae924476297c2cca
imuuu <imuuu@users.noreply.github.com>
2020-03-31 01:16:20 +0300

    Template added

--- Minecraft plugins/defaultPlugin/src/Interfaces/CommandInterface.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface CommandInterface 
-{
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args);
-}
